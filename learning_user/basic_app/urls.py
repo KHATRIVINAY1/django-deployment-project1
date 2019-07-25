fd5ee8bf83ff0ddef57e54a1f0660c59c0449d4e
KHATRIVINAY1 <48641471+KHATRIVINAY1@users.noreply.github.com>
2019-07-25 13:53:55 +0530

    Update urls.py

--- learning_user/basic_app/urls.py
+++ learning_user/basic_app/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 app_name = 'basic_app'
-urlpatterns = [path('',views.index,name='index'),
+urlpatterns = [path('',views.index,name='home'),
 				path('register/',views.register,name= 'register'),
+				path('user_login',views.user_login,name='user_login')
 	
 ]
