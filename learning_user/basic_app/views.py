acf8fa15fe5bb3b04805dc9b224ef12e65284bb8
KHATRIVINAY1 <48641471+KHATRIVINAY1@users.noreply.github.com>
2019-07-25 13:47:41 +0530

    Update views.py

--- learning_user/basic_app/views.py
+++ learning_user/basic_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .forms import UserInfo,UserProfileInfoForm
-from .models import UserProfileInfo
+
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login,logout
 # Create your views here.
 
 def index(request):
@@ -30,4 +34,30 @@ def register(request):
 
 
 	return render(request,'basic_app/register.html',{'registered':registered,'user_form':user_form,'profile_form':profile_form})
+
+
+@login_required
+def user_logout(request):
+	logout(request)
+	return index(request)
+
+def user_login(request):
+	if request.method =='POST':
+		username = request.POST.get('username')
+		password= request.POST.get('password')
+
+		user = authenticate(username= username, password=password)
+
+		if user:
+			if user.is_active:
+				login(request,user)
+				return index(request)
+			else:
+				return HttpResponse("Account not Active")
+		else:
+			print("Login is failed")
+			return HttpResponse("Invalid login detail supplied")
+	else:
+		return render(request,'basic_app/login.html',{})
+
 
